e4a46c05fe7a481c9ba10545f673c0f2563d471e
Jie Bo Ti <tijiebo@hotmail.com>
2018-01-08 18:02:37 +0800

    Added MapView

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import MapView from 'react-native-maps';
 import { View } from 'react-native';
 import {Header} from './common';
 
@@ -6,8 +7,17 @@ class App extends Component {
 
     render() {
         return (
-            <View>
+            <View style={{flex:1}}>
                 <Header headerText="PoGoMap"/>
+                <MapView
+                    style={{flex:1,alignItems:'center',justifyContent:'center',alignSelf:'stretch'}}
+                    initialRegion={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                />
             </View>
         );
     };
